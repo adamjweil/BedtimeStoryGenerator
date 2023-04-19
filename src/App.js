db3807fdca66b3e30eed351f2532852670ff704a
Adam Weil <adamjweil@gmail.com>
2023-04-18 18:14:54 -0600

    adding images

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import axios from 'axios';
 import './App.css'; // Import custom CSS file for additional styling
 import bgImg from './assets/bg_image.png';
 import icon from './assets/storytelling.png';
+import { BookHalf } from 'react-bootstrap-icons';
+
 
 const openai = new OpenAIApi({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
@@ -17,6 +19,36 @@ const App = () => {
   const [userInput, setUserInput] = useState('');
   const [generatedStory, setGeneratedStory] = useState('');
   const [isLoading, setIsLoading] = useState('');
+  const [currentParagraph, setCurrentParagraph] = useState(0);
+  const [generatedImages, setGeneratedImages] = useState([]);
+
+  useEffect(() => {
+    const generateImages = async () => {
+      const images = [];
+      for (let i = 0; i < generatedStory.length; i++) {
+        try {
+          const response = await axios.post(
+            "https://api.openai.com/v1/images/generations",
+            {
+              model: "image-alpha-001",
+              prompt: `Please take the following text and turn it into an image for a children's story:\n\n${generatedStory[i]}`,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+              },
+            }
+          );
+          images.push(response.data.data[0].url);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      setGeneratedImages(images);
+    };
+    generateImages();
+  }, [generatedStory]);
 
   // Function to handle user input change
   const handleInputChange = (event) => {
@@ -29,14 +61,21 @@ const App = () => {
     // Show loading spinner
     setIsLoading(true);
     console.log(userInput)
-    console.log("Is loading: " + isLoading)
     try {
         const response = await axios.post(
         "https://api.openai.com/v1/completions",
         {
           prompt: `
-            Write a children's bedtime story that is roughly 500 to 1000 words. One of the themes in the book should relate to ${userInput}. The story should have a beginning, middle and end. The beginning of the story should be about getting to know the main character. They should be fun and interesting. In the middle of the story they should deal with some sort of obstacle in their lives. The final section of the book will focus on how they overcame the obstacles in their lives, and that are better because of it. The story is for 7-10 year old children. By the end of the story the character should show some growth in their personalities and willingness to accept change.
-          `,
+          Write a children's bedtime story that is roughly 500 to 1000 words.
+          One of the themes in the story should relate to ${userInput}.
+          The story should have a beginning, middle and end.
+          The beginning of the story should be about getting to know the main character. They should have a fun personality and interesting perspective.
+          The middle of the story should be about the main character enduring some sort of obstacle in their lives.
+          The final section of the story should focus on their triumph of said obstacle. They should be anecdotes of how they are better because of it.
+          By the end of the story the character should show some growth in their personalities and willingness to accept change. This can be represented in the story via abstract references, or more directly.
+          There should be elements of humor in the story.
+          Choose words that draw a stark imagery.
+        `,
           model: "text-davinci-003",
           max_tokens: 1000,
           temperature: 1,
@@ -48,21 +87,34 @@ const App = () => {
           },
         }
        );
-       const generatedText = response.data.choices[0].text.trim();
-      setGeneratedStory(generatedText);
+       console.log(response.data.choices)
+          const generatedText = response.data.choices[0].text;
+          const generatedStoryArray = generatedText.split('\n\n');
+          setGeneratedStory(generatedStoryArray);
+          setCurrentParagraph(0);
     } catch (error) {
       console.error(error);
     } finally {
       // Hide loading spinner
         setIsLoading(false);
-        console.log("Is loading: " + isLoading);
     }
   };
 
   // Function to clear generated story
   const clearStory = () => {
-    setGeneratedStory('');
+    setGeneratedStory([]);
+    setGeneratedImages([]);
     setUserInput('');
+  }
+
+  // Function to handle next paragraph button click
+  const handleNextClick = () => {
+    setCurrentParagraph(currentParagraph + 1);
+  }
+
+  // Function to handle previous paragraph button click
+  const handlePrevClick = () => {
+    setCurrentParagraph(currentParagraph - 1);
   }
 
   return (
@@ -79,8 +131,11 @@ const App = () => {
       }}>
         <Col xs={12} sm={8} md={6} lg={4}>
         <div className="title-container">
+          <h1 className="title" style={{whiteSpace: 'nowrap'}}>
+             Bedtime Story
+          </h1>
           <h1 className="title">
-             Bedtime Story Generator
+              Generator
           </h1>
           </div>
           <Form style={{textAlign: "center"}}>
@@ -88,7 +143,7 @@ const App = () => {
             <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>              <Form.Group controlId="userInput" style={{textAlign: "center", justifyContent: 'center'}}>
                 <Form.Control
                   type="text"
-                  placeholder="Enter your story prompt here"
+                  placeholder="Enter your story prompt here..."
                   value={userInput}
                   onChange={handleInputChange}
                   style={{
@@ -101,26 +156,24 @@ const App = () => {
                     paddingRight: '48px',
                     height: '48px',
                     margin: '16px 0',
-                    width: '250px',
+                    width: '350px',
                   }}
                 />
               </Form.Group>
               <div style={{ margin: '20px' }}>
-                <Button variant="secondary" onClick={generateStory}>
-                  Generate
+                <Button size='sm' variant="primary" style={{borderRadius: '10px', padding: '8px', border: '1px white solid'}} onClick={generateStory}>
+                   <BookHalf style={{marginRight: '5px'}}/> GENERATE
                 </Button>
               </div>
             </div>
           </Form>
           </Col>
+
           </Row>
 
-          <div class="container">
-            <div class="left-column">
 
-            </div>
-            <div class="middle-column">
-            <Row>
+
+            <Row style ={{marginLeft: '10%', marginRight: '10%', marginTop: '30px'}}>
             <Col>
             {isLoading ? (
               <Button style={{}} variant='primary' disabled>
@@ -133,12 +186,33 @@ const App = () => {
               </Button>
 
             ) : (
-              generatedStory && (
+              generatedStory && generatedStory.length > 0 && (
                 <div className="output-field">
-
                   <div className="notebook">
-
-                    {generatedStory}
+                  <h2 style={{ marginBottom: '20px' }}>Story</h2>
+                  <div>
+                    <img style={{width: '300px', height: '300'}} src={generatedImages[currentParagraph]} alt={`Image for paragraph ${currentParagraph}`} />
+                  </div>
+                  <p>{generatedStory[currentParagraph]}</p>
+                  </div>
+                  <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                    <Button
+                      variant='primary'
+                      size='sm'
+                      onClick={handlePrevClick}
+                      disabled={currentParagraph === 0}
+                      style={{ marginRight: '10px' }}
+                    >
+                      Prev
+                    </Button>
+                    <Button
+                      variant='primary'
+                      size='sm'
+                      onClick={handleNextClick}
+                      disabled={currentParagraph === generatedStory.length - 1}
+                    >
+                      Next
+                    </Button>
                   </div>
                   <Button variant="clear-button" onClick={clearStory}>
                     Clear
@@ -148,10 +222,8 @@ const App = () => {
             )}
             </Col>
             </Row>
-            </div>
-            <div class="right-column">
-            </div>
-          </div>
+
+
 
     </Container>
   );
