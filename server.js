50f9b7fef382b9ad4fbad6fda1507e13665bba07
Adam Weil <adamjweil@gmail.com>
2023-04-23 11:34:07 -0600

    adding an express mongoose backend to support eventual user sign up

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
